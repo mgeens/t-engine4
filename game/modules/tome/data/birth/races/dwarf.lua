991b7a7a2195e99fa013ca7f2b5cf814123f0b13
Shibari <ShibariTOME@Gmail.com>
2018-11-22 15:09:55 -0500

    Fix
    How did I even do this

--- game/modules/tome/data/birth/races/dwarf.lua
+++ game/modules/tome/data/birth/races/dwarf.lua
@@ -79,7 +79,6 @@ newBirthDescriptor
 		random_name_def = "dwarf_#sex#",
 		life_rating = 12,
 		can_see_iron_council = 1,
-		life_rating=12,
 		resolvers.inscription("INFUSION:_REGENERATION", {cooldown=10, dur=5, heal=100}),
 		resolvers.inscription("INFUSION:_WILD", {cooldown=14, what={physical=true}, dur=4, power=14}),
 		resolvers.inventory({id=true, transmo=false, alter=function(o) o.inscription_data.cooldown=12 o.inscription_data.heal=50 end, {type="scroll", subtype="infusion", name="healing infusion", ego_chance=-1000, ego_chance=-1000}}),
