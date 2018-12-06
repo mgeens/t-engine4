ace91e5db79806fee40f67f648f4afcf6ad90a64
Shibari <ShibariTOME@Gmail.com>
2018-12-06 12:02:43 -0500

    Add mana, vim, negative, and positive displays to Actor tooltips if the player knows Antimagic

--- game/modules/tome/class/Actor.lua
+++ game/modules/tome/class/Actor.lua
@@ -1901,6 +1901,22 @@ function _M:tooltip(x, y, seen_by)
 	else ts:add({"color", 0, 255, 255}, ("Level: %d"):format(self.level), {"color", "WHITE"}, true) end
 	if self:attr("invulnerable") then ts:add({"color", "PURPLE"}, "INVULNERABLE!", true) end
 	ts:add({"color", 255, 0, 0}, ("HP: %d (%d%%) #GREEN#+%0.2f#LAST#"):format(self.life, self.life * 100 / self.max_life, self.life_regen * util.bound(self.healing_factor or 1)), {"color", "WHITE"})
+	
+	-- Avoid cluttering tooltip if resources aren't relevant (add menu option?)
+	if game.player:knowTalentType("wild-gift/antimagic") then
+		if self:knowTalent(self.T_MANA_POOL) then 
+			ts:add(("\nMana:  "..self.resources_def.mana.color.."%d / %d#LAST#"):format(self.mana, self.max_mana, true))
+		end
+		if self:knowTalent(self.T_VIM_POOL) then
+			ts:add(("\nVim:  "..self.resources_def.vim.color.."%d / %d#LAST#"):format(self.vim, self.max_vim, true))
+		end
+		if self:knowTalent(self.T_POSITIVE_POOL) then
+			ts:add(("\nPositive:  "..self.resources_def.positive.color.."%d / %d#LAST#"):format(self.positive, self.max_positive, true))
+		end
+		if self:knowTalent(self.T_NEGATIVE_POOL) then
+			ts:add(("\nNegative:  "..self.resources_def.negative.color.."%d / %d#LAST#"):format(self.negative, self.max_negative, true))
+		end
+	end
 
 	if self:knowTalent(self.T_SOLIPSISM) then
 		local psi_percent = 100*self.psi/self.max_psi
