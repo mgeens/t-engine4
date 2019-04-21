b867ca1a02a7983a5a103de9d90db6d1bffcbc43
DarkGod <darkgod@net-core.org>
2019-04-21 22:14:50 +0200

    plop

--- utils/giffy.sh
+++ utils/giffy.sh
@@ -12,6 +12,7 @@ duration=$4
 
 palette="/tmp/palette.png"
 
+# filters="fps=20,scale=400:-1"
 filters="fps=15"
 
 /Test/ffmpeg-3.2-64bit-static/ffmpeg -v warning -ss $start_time -t $duration -i $src -vf "$filters,palettegen" -y $palette
