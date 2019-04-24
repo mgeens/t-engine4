12de7977f42ebacc801faac7bec734ec8f6995de
Shibari <ShibariTOME@Gmail.com>
2019-04-24 14:18:38 -0400

    Round most talent masteries to 0.0 or 0.3
    Masteries are a way to reinforce the thematic strengths of a class.  Its hard to really "feel" a difference when you're changing things in increments of 0.1 instead of 0.3.
    
    Additionally, this helps people memorize differences in breakpoints between classes.

--- game/modules/tome/data/birth/classes/mage.lua
+++ game/modules/tome/data/birth/classes/mage.lua
@@ -88,7 +88,7 @@ newBirthDescriptor{
 		["spell/frost-alchemy"]={true, 0.3},
 		["spell/energy-alchemy"]={false, 0.3},
 		["spell/staff-combat"]={true, 0.3},
-		["cunning/survival"]={false, -0.1},
+		["cunning/survival"]={false, 0.0},
 	},
 	talents = {
 		[ActorTalents.T_CREATE_ALCHEMIST_GEMS] = 1,
@@ -197,7 +197,7 @@ newBirthDescriptor{
 		["spell/divination"]={true, 0.3},
 		["spell/conveyance"]={true, 0.3},
 		["spell/aegis"]={true, 0.3},
-		["cunning/survival"]={false, -0.1},
+		["cunning/survival"]={false, 0.0},
 	},
 	unlockable_talents_types = {
 		["spell/wildfire"]={false, 0.3, "mage_pyromancer"},
@@ -255,8 +255,8 @@ newBirthDescriptor{
 	power_source = {arcane=true},
 	stats = { mag=5, wil=3, cun=1, },
 	talents_types = {
-		["spell/conveyance"]={true, 0.2},
-		["spell/divination"]={true, 0.2},
+		["spell/conveyance"]={true, 0.3},
+		["spell/divination"]={true, 0.3},
 		["spell/necrotic-minions"]={true, 0.3},
 		["spell/advanced-necrotic-minions"]={false, 0.3},
 		["spell/shades"]={false, 0.3},
@@ -264,7 +264,7 @@ newBirthDescriptor{
 		["spell/nightfall"]={true, 0.3},
 		["spell/grave"]={true, 0.3},
 		["spell/animus"]={true, 0.3},
-		["cunning/survival"]={true, -0.1},
+		["cunning/survival"]={true, 0.0},
 	},
 	unlockable_talents_types = {
 		["spell/ice"]={false, 0.2, "mage_cryomancer"},
