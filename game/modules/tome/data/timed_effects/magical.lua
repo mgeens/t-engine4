34f9f2bf6039a8c684663142dad06467747e1fd2
Shibari <ShibariTOME@Gmail.com>
2019-05-22 06:25:34 -0400

    Lower the cap on Out of Phase to 40%
    Its a pretty safe bet this was still broken even without PD.  40% is a lot still.

--- game/modules/tome/data/timed_effects/magical.lua
+++ game/modules/tome/data/timed_effects/magical.lua
@@ -2352,7 +2352,7 @@ newEffect{
 newEffect{
 	name = "OUT_OF_PHASE", image = "talents/phase_door.png",
 	desc = "Out of Phase",
-	long_desc = function(self, eff) return ("The target is out of phase with reality, increasing defense by %d, resist all by %d%%, and reducing the duration of detrimental timed effects by %d%%.\nThese effects cap at 60%%."):format(eff.defense or 0, eff.resists or 0, eff.effect_reduction or 0) end,
+	long_desc = function(self, eff) return ("The target is out of phase with reality, increasing defense by %d, resist all by %d%%, and reducing the duration of detrimental timed effects by %d%%.\nThese effects cap at 40%%."):format(eff.defense or 0, eff.resists or 0, eff.effect_reduction or 0) end,
 	type = "magical",
 	subtype = { teleport=true },
 	status = "beneficial",
@@ -2360,9 +2360,9 @@ newEffect{
 	on_gain = function(self, err) return "#Target# is out of phase.", "+Phased" end,
 	on_lose = function(self, err) return "#Target# is no longer out of phase.", "-Phased" end,
 	activate = function(self, eff)
-		eff.defense = math.min(60, eff.defense + (self:attr("defense_on_teleport") or 0))
-		eff.resists = math.min(60, eff.resists + (self:attr("resist_all_on_teleport") or 0))
-		eff.effect_reduction = math.min(60, eff.effect_reduction + (self:attr("effect_reduction_on_teleport") or 0))
+		eff.defense = math.min(40, eff.defense + (self:attr("defense_on_teleport") or 0))
+		eff.resists = math.min(40, eff.resists + (self:attr("resist_all_on_teleport") or 0))
+		eff.effect_reduction = math.min(40, eff.effect_reduction + (self:attr("effect_reduction_on_teleport") or 0))
 
 		eff.defid = self:addTemporaryValue("combat_def", eff.defense)
 		eff.resid= self:addTemporaryValue("resists", {all=eff.resists})
@@ -2370,9 +2370,9 @@ newEffect{
 		eff.particle = self:addParticles(Particles.new("phantasm_shield", 1))
 	end,
 	on_merge = function(self, old_eff, new_eff)
-		old_eff.defense = math.min(60, math.max(old_eff.defense, new_eff.defense)) or 0
-		old_eff.resists = math.min(60, math.max(old_eff.resists, new_eff.resists)) or 0
-		old_eff.effect_reduction = math.min(60, math.max(old_eff.effect_reduction, new_eff.effect_reduction)) or 0
+		old_eff.defense = math.min(40, math.max(old_eff.defense, new_eff.defense)) or 0
+		old_eff.resists = math.min(40, math.max(old_eff.resists, new_eff.resists)) or 0
+		old_eff.effect_reduction = math.min(40, math.max(old_eff.effect_reduction, new_eff.effect_reduction)) or 0
 
 		self:removeTemporaryValue("combat_def", old_eff.defid)
 		self:removeTemporaryValue("resists", old_eff.resid)
