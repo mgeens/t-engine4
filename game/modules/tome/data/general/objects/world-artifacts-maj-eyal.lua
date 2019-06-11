e162eced1baa26b54100afa1049c2aaa96651f69
Shibari <ShibariTOME@Gmail.com>
2019-06-11 00:56:32 -0400

    Revise Guidance back to closer to the old 1.6 version
    I think high masteries are more interesting than replicating AMS, but lets keep the Arcane resist for theme.

--- game/modules/tome/data/general/objects/world-artifacts-maj-eyal.lua
+++ game/modules/tome/data/general/objects/world-artifacts-maj-eyal.lua
@@ -426,10 +426,9 @@ It seems somebody well versed in antimagic could use it to its fullest potential
 		combat_physresist = 15,
 		combat_mentalresist = 15,
 		combat_spellresist = 15,
-		talents_types_mastery = { ["wild-gift/call"] = 0.2, ["wild-gift/antimagic"] = 0.2, },
+		talents_types_mastery = { ["wild-gift/call"] = 0.2, ["wild-gift/antimagic"] = 0.5, },
 		resists_cap = { [DamageType.BLIGHT] = 10, },
 		resists = { [DamageType.BLIGHT] = 20, },
-		flat_damage_armor = {all=30, [DamageType.PHYSICAL] = -30, [DamageType.MIND] = -30 },
 	},
 	on_wear = function(self, who)
 		if who:attr("forbid_arcane") then
@@ -439,7 +438,7 @@ It seems somebody well versed in antimagic could use it to its fullest potential
 			self:specialWearAdd({"wielder","inc_stats"}, { [Stats.STAT_WIL] = 6, [Stats.STAT_CUN] = 6, [Stats.STAT_CON] = 6})
 			self:specialWearAdd({"wielder","combat_spellresist"}, 15)
 			self:specialSetAdd({"wielder","equilibrium_regen"}, -1)
-			self:specialSetAdd({"wielder","resists"}, {[engine.DamageType.ARCANE]=20})
+			self:specialSetAdd({"wielder","resists"}, {[engine.DamageType.ARCANE]=40})
 			self:specialSetAdd({"wielder","resists_cap"}, {[engine.DamageType.ARCANE]=10})			
 			game.logPlayer(who, "#LIGHT_BLUE#You feel a great hero guiding you!")
 		end
