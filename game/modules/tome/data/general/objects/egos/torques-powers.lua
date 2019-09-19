927d05161c9c0d59e28985b209a943b528c16cdd
Shibari <ShibariTOME@Gmail.com>
2019-09-19 19:57:27 -0400

    Fix Gale Force damage type display

--- game/modules/tome/data/general/objects/egos/torques-powers.lua
+++ game/modules/tome/data/general/objects/egos/torques-powers.lua
@@ -72,7 +72,7 @@ newEntity{
 	charm_power_def = {add=0, max=800, floor=true},
 	resolvers.charm(
 		function(self, who)
-			local dam = who:damDesc(engine.DamageType.Mind, self.use_power.damage(self, who))
+			local dam = who:damDesc(engine.DamageType.PHYSICAL, self.use_power.damage(self, who))
 			return ("project a gust of wind in a cone knocking enemies back %d spaces and dealing %d damage"):format(self.use_power.knockback(self, who), dam)
 		end,
 		15,
