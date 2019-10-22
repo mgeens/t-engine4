f555d62f66e22dd01e48dd09ed9602982f0e5714
Shibari <ShibariTOME@Gmail.com>
2019-10-22 17:26:34 -0400

    Shadow Grasp no longer movies enemies if they're adjacent

--- game/modules/tome/data/talents/cunning/ambush.lua
+++ game/modules/tome/data/talents/cunning/ambush.lua
@@ -76,10 +76,11 @@ newTalent{
 		local sx, sy = util.findFreeGrid(self.x, self.y, 5, true, {[engine.Map.ACTOR]=true})
 		if not sx then return end
 
-		-- Move first so we get the full benefit of Shadowstrike
-		target:move(sx, sy, true)
-		self:project(tg, target.x, target.y, DamageType.DARKNESS, self:spellCrit(t.getDamage(self, t)))
-
+		if core.fov.distance(self.x, self.y, target.x, target.y) > 1 then
+			-- Move first so we get the full benefit of Shadowstrike
+			target:move(sx, sy, true)
+			self:project(tg, target.x, target.y, DamageType.DARKNESS, self:spellCrit(t.getDamage(self, t)))
+		end
 		if target:canBe("silence") then
 			target:setEffect(target.EFF_SILENCED, t.getDuration(self, t), {apply_power=self:combatAttack()})
 		else
