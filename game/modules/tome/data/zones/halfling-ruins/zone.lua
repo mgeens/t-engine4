89043a9521db06b7e4ff3f9e95ac3b25dd09902d
Bunny <glisa825@gmail.com>
2019-12-28 06:58:39 -0500

    Starsapphire living weapons Shows everywhere greater vaults are found plus halfling ruins

--- game/modules/tome/data/zones/halfling-ruins/zone.lua
+++ game/modules/tome/data/zones/halfling-ruins/zone.lua
@@ -35,8 +35,9 @@ return {
 		map = {
 			class = "engine.generator.map.Roomer",
 			nb_rooms = 10,
-			rooms = {"random_room", {"money_vault",5}, {"lesser_vault",8}},
-			lesser_vaults_list = {"circle","rat-nest","skeleton-mage-cabal","worms"},
+			rooms = {"random_room", {"money_vault",5}, {"lesser_vault",8}, {"greater_vault",10}},
+			lesser_vaults_list = {"circle","rat-nest","skeleton-mage-cabal"},
+			greater_vaults_list = {"living-weapons"},
 			lite_room_chance = 100,
 			['.'] = "FLOOR",
 			['#'] = "WALL",
