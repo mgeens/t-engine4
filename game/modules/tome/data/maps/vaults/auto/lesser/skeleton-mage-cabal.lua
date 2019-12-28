45875be9a99d978e4827d7ae59dd834fb76c6901
Bunny <glisa825@gmail.com>
2019-12-28 03:42:17 -0500

    Nsrr mage cabal update Same spawn locations as before, added variety

--- game/modules/tome/data/maps/vaults/auto/lesser/skeleton-mage-cabal.lua
+++ game/modules/tome/data/maps/vaults/auto/lesser/skeleton-mage-cabal.lua
@@ -19,11 +19,38 @@
 
 setStatusAll{no_teleport=true, vault_only_door_open=true, room_map = {can_open=true}}
 roomCheck(function(room, zone, level, map)
-	return resolvers.current_level >= 5 and resolvers.current_level <= 25
+   return resolvers.current_level >= 5 and resolvers.current_level <= 25
 end)
 specialList("actor", {
-	"/data/general/npcs/skeleton.lua",
+   "/data/general/npcs/skeleton.lua",
+   "/data/general/npcs/thieve.lua",
+   "/data/general/npcs/horror.lua",
 })
+specialList("terrain", {
+   "/data/general/grids/forest.lua",
+}, true)
+local mobs = {
+   "skeleton mage",
+   "skeleton warrior",
+   "skeleton archer",
+   "thief",
+   "bloated horror",
+}
+local mob = rng.tableRemove(mobs)
+
+local weapon = "staff"
+if mob == "skeleton warrior" then weapon = rng.percent(50) and "waraxe" or "battleaxe"
+elseif   mob == "skeleton archer" then weapon = "longbow"
+elseif   mob == "thief" then weapon = rng.percent(50) and "dagger" or "sling"
+elseif   mob == "bloated horror" then weapon = "mindstar"
+end
+
+local armor = "cloth"
+if mob == "skeleton warrior" then armor = rng.percent(50) and "shield" or "massive"
+elseif   mob == "skeleton archer" then armor = rng.percent(50) and "light" or "heavy"
+elseif   mob == "thief" then armor = "light"
+end
+
 border = 0
 startx, starty = 4, 10
 defineTile('.', data.floor or "FLOOR")
@@ -31,22 +58,22 @@ defineTile(',', "FLOOR")
 defineTile('D', "DOOR")
 defineTile('!', "DOOR_VAULT")
 defineTile('X', "HARDWALL")
-defineTile('W', "FLOOR", {random_filter={subtype="staff", tome_mod="vault", add_levels=4}})
-defineTile('S', "FLOOR", {random_filter={type="scroll", ego_chance=25, add_levels=4}})
-defineTile('M', "FLOOR", nil, {random_filter={name="skeleton mage", add_levels=4}})
-
+defineTile('S', "FLOOR", {random_filter={type="scroll", ego_chance=50, add_levels=6}})
+defineTile('W', "FLOOR", {random_filter={subtype=weapon, tome_mod="vault", add_levels=6, ego_chance=100}})
+defineTile('A', "FLOOR", {random_filter={subtype=armor, tome_mod="vault", add_levels=6, ego_chance=100}})
+defineTile('M', "FLOOR", nil, {random_filter={name=mob, add_levels=8}})
 rotates = {"default", "90", "180", "270", "flipx", "flipy"}
 
 return {
 [[.........]],
 [[.XXXXXXX.]],
-[[.XSSSSSX.]],
+[[.XASSSWX.]],
 [[.XXXDXXX.]],
 [[.X,,M,,X.]],
 [[.X,M,M,X.]],
 [[.X,,M,,X.]],
 [[.XDX!XDX.]],
-[[.XWX.XWX.]],
+[[.XWX.XAX.]],
 [[.XXX.XXX.]],
 [[.........]],
 }
