1ecb855d1c84d02ecbd98d739895448e39930ba6
DarkGod <darkgod@net-core.org>
2020-01-12 18:22:07 +0100

    plop

--- game/modules/tome/data/damage_types.lua
+++ game/modules/tome/data/damage_types.lua
@@ -126,7 +126,7 @@ setDefaultProjector(function(src, x, y, type, dam, state)
 		local ignore_direct_crits = target:attr 'ignore_direct_crits'
 		if crit_power > 1 and ignore_direct_crits then -- Reduce the post crit damage, we have to do this here since most crits are calculated before knowing their target
 			dam = dam / crit_power
-			local reduce = (crit_power - 1) * (ignore_direct_crits / 100)
+			local reduce = (crit_power - 1) * (util.bound(ignore_direct_crits, 0, 100) / 100)
 			crit_power = math.max(1, crit_power - reduce)
 			dam = dam * crit_power
 			print("[PROJECTOR] crit power reduce dam", dam)
